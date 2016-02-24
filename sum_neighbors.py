d0eb60df66372bb005cda351bd18317e908d73b9
jbonilla <jbonilla@caltech.edu>
2016-02-23 23:41:37 -0800

    remvoed print

--- sum_neighbors.py
+++ sum_neighbors.py
@@ -24,7 +24,6 @@ def generate_seeds(num_players, num_seeds, G):
 	sorted_degrees = sorted(sum_degrees, key=itemgetter(1), reverse=True)
 	for i in range(num_seeds):
 		seeds[i] = sorted_degrees[i][0]
-		print seeds[i]
 
 	return seeds
 
