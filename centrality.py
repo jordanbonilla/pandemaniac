d18d82429437fbbe14b2cd8f2ba51749ec06c824
gcarlson <gcarlson@caltech.edu>
2016-02-23 22:56:41 -0800

    Added centrality method for finding seeds.

---
+++ centrality.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python2
+
+import networkx as nx
+import numpy as np
+from operator import itemgetter
+
+def generate_seeds(num_players, num_seeds, G):
+    # Initialize see array to zeros
+    seeds = np.zeros(num_seeds, dtype=np.int)
+
+    m = nx.closeness_centrality(G);
+
+    sorted_centralities = sorted(m.items(), key=itemgetter(1))
+
+    for i in range(num_seeds):
+        seeds[i] = sorted_centralities[i][0]
+
+    return seeds
