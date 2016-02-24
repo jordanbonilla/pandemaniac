a0cd6ec66d30991e04c1500a1766d49620dd38e0
jordanbonilla <jbonilla@caltech.edu>
2016-02-23 23:38:38 -0800

    reverse sort

--- sum_neighbors.py
+++ sum_neighbors.py
@@ -9,7 +9,7 @@ def generate_seeds(num_players, num_seeds, G):
 
 	neighbors = nx.to_dict_of_lists(G).values()
 
-	degrees = seeds = np.zeros(len(neighbors), dtype=np.int)
+	degrees = np.zeros(len(neighbors), dtype=np.int)
 	sum_degrees = [(None, None)] * len(neighbors)  # tuple of (node_id, degree)
 	# Assign degrees to each node in the graph
 	for i in range(len(neighbors)):
@@ -21,9 +21,10 @@ def generate_seeds(num_players, num_seeds, G):
 		for j in range(len(neighbors[i])):
 			sum = sum + degrees[this_nodes_neighbors[j]]
 		sum_degrees[i] = ( i, sum )
-	sorted_degrees = sorted(sum_degrees, key=itemgetter(1))
+	sorted_degrees = sorted(sum_degrees, key=itemgetter(1), reverse=True)
 	for i in range(num_seeds):
 		seeds[i] = sorted_degrees[i][0]
+		print seeds[i]
 
 	return seeds
 
